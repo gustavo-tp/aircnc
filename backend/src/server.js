d24d634ee8b966142a99257409579293d43d16ca
gustavo-tp <gustavo1190.gt@gmail.com>
2020-01-09 12:50:15 -0300

    Frontend concluído.

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
+const path = require('path');
 
 const routes = require('./routes');
 
 const app = express();
 
 mongoose.connect(
-    'mongodb+srv://omnistack:omnistack@cluster0-ztlrw.mongodb.net/semana09?retryWrites=true&w=majority',
-    {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    }
+  'mongodb+srv://omnistack:omnistack@cluster0-ztlrw.mongodb.net/semana09?retryWrites=true&w=majority',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
 );
 
 // GET, POST, PUT, DELETE
@@ -19,7 +21,9 @@ mongoose.connect(
 // req.params = Acessar route params (para edição e delete)
 // req.body = Acessar corpo da requisição (para criação, edição)
 
+app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
 app.listen(3333);
