19b9be1a07068b1d3b56f0048c8d7ce428e816c8
gustavo-tp <gustavo1190.gt@gmail.com>
2020-01-10 14:33:19 -0300

    Adicionado mensagem de solicitação e reposta de reserva de spot em tempo real com socket.io.

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,9 +3,14 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const path = require('path');
 
+const socketio = require('socket.io');
+const http = require('http');
+
 const routes = require('./routes');
 
 const app = express();
+const server = http.Server(app);
+const io = socketio(server);
 
 mongoose.connect(
   'mongodb+srv://omnistack:omnistack@cluster0-ztlrw.mongodb.net/semana09?retryWrites=true&w=majority',
@@ -14,6 +19,21 @@ mongoose.connect(
     useUnifiedTopology: true
   }
 );
+
+const connectedUsers = {};
+
+io.on('connection', socket => {
+  const { user_id } = socket.handshake.query;
+
+  connectedUsers[user_id] = socket.id;
+});
+
+app.use((req, res, next) => {
+  req.io = io;
+  req.connectedUsers = connectedUsers;
+
+  return next();
+});
 
 // GET, POST, PUT, DELETE
 
@@ -26,4 +46,4 @@ app.use(express.json());
 app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
-app.listen(3333);
+server.listen(3333);
