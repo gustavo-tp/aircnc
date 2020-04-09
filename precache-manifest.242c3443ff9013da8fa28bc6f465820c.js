c71d53892a1e4a713ed6f1ffd07555c9d4e0e926
gustavo-tp <gustavo1190.gt@gmail.com>
2020-04-08 23:11:03 -0300

    Updates

---
+++ precache-manifest.242c3443ff9013da8fa28bc6f465820c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2de12858443b779e8a32d953d0e6b50",
+    "url": "/aircnc/index.html"
+  },
+  {
+    "revision": "706254302cc5ff1b2fad",
+    "url": "/aircnc/static/css/main.5cec01ce.chunk.css"
+  },
+  {
+    "revision": "02f46c187d18ca70eb2b",
+    "url": "/aircnc/static/js/2.f5231d95.chunk.js"
+  },
+  {
+    "revision": "e71e6872c2c23a0effc1ae2a720ff822",
+    "url": "/aircnc/static/js/2.f5231d95.chunk.js.LICENSE"
+  },
+  {
+    "revision": "706254302cc5ff1b2fad",
+    "url": "/aircnc/static/js/main.d475a7c6.chunk.js"
+  },
+  {
+    "revision": "8e42fb897ab8af1b0530",
+    "url": "/aircnc/static/js/runtime-main.fbdb279c.js"
+  },
+  {
+    "revision": "83252e0afaf5f134063345abd6ac8fd7",
+    "url": "/aircnc/static/media/background.83252e0a.jpg"
+  },
+  {
+    "revision": "6a6f4b6ebebfabdc283a3a6b417282a0",
+    "url": "/aircnc/static/media/camera.6a6f4b6e.svg"
+  },
+  {
+    "revision": "740528028131fdef515dcb930373ccf6",
+    "url": "/aircnc/static/media/logo.74052802.svg"
+  }
+]);
